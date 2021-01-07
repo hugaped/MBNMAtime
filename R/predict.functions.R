cc7d8851208c47a13d765807590176b7ae010517
Hugo <hugo.pedder@bristol.ac.uk>
2021-01-07 19:38:08 +0000

    removed print

--- R/predict.functions.R
+++ R/predict.functions.R
@@ -292,7 +292,6 @@ predict.mbnma <- function(object, times=c(0:max(object$model.arg$jagsdata$time, 
   if (any(c("rcs", "bs", "ns") %in% object$model.arg$fun)) {
     spline <- genspline(times, spline=object$model.arg$fun$name, knots=object$model.arg$fun$knots)
   }
-  print(timecourse)
 
   ########## Predict responses ###########
 
