ed73c50cf5524858f3ecc47cf8462706a3375763
Hugo <hugo.pedder@bristol.ac.uk>
2021-03-29 15:21:16 +0100

    removed unused global variables

--- R/globals.R
+++ R/globals.R
@@ -5,5 +5,4 @@
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1") utils::globalVariables(c("studyID", "time", "treatment", "fup", "study", "arm", "fupcount", "y",
                                                        "2.5%", "50%", "97.5%", "ref.median",
-                                                       "beta.1.str", "beta.2.str", "beta.3.str", "beta.4.str",
                                                        "int.fun", ".data"))
