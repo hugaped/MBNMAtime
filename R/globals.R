3c8761d35b82d7bef277082ab7f60ff36a22820b
Hugo <hugo.pedder@bristol.ac.uk>
2021-09-12 22:59:18 +0100

    updated globals

--- R/globals.R
+++ R/globals.R
@@ -3,6 +3,8 @@
 # Date created: 2018-09-10
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1") utils::globalVariables(c("studyID", "time", "treatment", "fup", "fups", "study", "arm", "fupcount", "y",
+if(getRversion() >= "2.15.1") utils::globalVariables(c("studyID", "time", "treatment",
+                                                       "fup", "fups", "study", "arm", "fupcount", "y",
                                                        "2.5%", "50%", "97.5%", "ref.median",
+                                                       "Var1", "value", "Parameter", ".",
                                                        "int.fun", ".data"))
