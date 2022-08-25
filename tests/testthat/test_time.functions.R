bec80a7dc707cd2d2909a49ab32e8c9d64d19668
Hugo Pedder <hugo.pedder@bristol.ac.uk>
2022-08-25 16:38:30 +0100

    tests pass

--- tests/testthat/test_time.functions.R
+++ tests/testthat/test_time.functions.R
@@ -16,19 +16,22 @@ testthat::test_that("tloglin functions correctly", {
 
 
 testthat::test_that("temax functions correctly", {
-  timefun <- temax(pool.emax="rel", method.emax="common", pool.et50="rel", method.et50="common")
+
+  # Warnings suppressed for version 0.2.2
+
+  timefun <- suppressWarnings(temax(pool.emax="rel", method.emax="common", pool.et50="rel", method.et50="common"))
   expect_equal(timefun$nparam, 2)
 
-  timefun <- temax(pool.emax="rel", method.emax="common", pool.et50="rel", method.et50="common",
-                  pool.hill="rel", method.hill="common")
+  timefun <- suppressWarnings(temax(pool.emax="rel", method.emax="common", pool.et50="rel", method.et50="common",
+                  pool.hill="rel", method.hill="common"))
   expect_equal(timefun$nparam, 3)
 
-  expect_message(temax(pool.emax="rel", method.emax="common", pool.et50="rel", method.et50="common"), "et50")
-  expect_message(temax(pool.emax="rel", method.emax="common", pool.et50="rel", method.et50="common",
-                       pool.hill="abs", method.hill="random"), "hill")
+  expect_message(suppressWarnings(temax(pool.emax="rel", method.emax="common", pool.et50="rel", method.et50="common")), "et50")
+  expect_message(suppressWarnings(temax(pool.emax="rel", method.emax="common", pool.et50="rel", method.et50="common",
+                       pool.hill="abs", method.hill="random")), "hill")
 
-  timefun <- temax(pool.emax="abs", method.emax="random", pool.et50="rel", method.et50="random",
-                   pool.hill="abs", method.hill="common")
+  timefun <- suppressWarnings(temax(pool.emax="abs", method.emax="random", pool.et50="rel", method.et50="random",
+                   pool.hill="abs", method.hill="common"))
   expect_equal(timefun$apool, c(emax="abs", et50="rel", hill="abs"))
   expect_equal(timefun$amethod, c(emax="random", et50="random", hill="common"))
 
--- tests/testthat/test_write.functions.R
+++ tests/testthat/test_write.functions.R
@@ -7,10 +7,17 @@ testthat::test_that("testing prior writing functions", {
   skip_on_ci()
   skip_on_cran()
 
-  emax1 <- mb.run(network,
+  emax1 <- suppressWarnings(mb.run(network,
                   fun=temax(pool.emax="rel", method.emax="random",
                             pool.et50="rel", method.et50="common"),
-                  n.chain=3, n.iter=200, n.burnin=100)
+                  corparam = TRUE,
+                  n.chain=3, n.iter=200, n.burnin=100))
+
+  emax2 <- suppressWarnings(mb.run(network,
+                                   fun=temax(pool.emax="rel", method.emax="random",
+                                             pool.et50="rel", method.et50="common"),
+                                   corparam = FALSE,
+                                   n.chain=3, n.iter=200, n.burnin=100))
 
   ############### Testing prior writing functions ###############
 
@@ -18,6 +25,8 @@ testthat::test_that("testing prior writing functions", {
     testthat::expect_equal(sort(names(get.prior(emax1$model.arg$jagscode))), sort(c("muinv.R", "alpha", "sd.emax", "inv.R")))
     testthat::expect_equal(class(get.prior(emax1$model.arg$jagscode)), "list")
     testthat::expect_equal(class(get.prior(emax1$model.arg$jagscode)[[1]]), "character")
+
+    testthat::expect_equal(sort(names(get.prior(emax2$model.arg$jagscode))), sort(c("mu.1", "mu.2", "alpha", "sd.emax", "et50", "emax")))
 
     priors <- get.prior(emax1$model.arg$jagscode)
     for (i in seq_along(priors)) {
@@ -78,7 +87,7 @@ testthat::test_that("test.mb.write", {
   jags <- mb.write(fun=tpoly(degree=2, pool.1 = "rel", method.1="common",
                              pool.2="abs", method.2="random"),
                    rho="dunif(-1,1)", covar="AR1")
-  testthat::expect_equal(length(grep("rho ~ dunif\\(-1,1", jags))>0, TRUE)
+  testthat::expect_equal(length(grep("rho ~ dunif\\(-?[0-9],1", jags))>0, TRUE)
   testthat::expect_equal(length(grep("cor\\[i,r,c\\]", jags))>0, TRUE)
   testthat::expect_equal(length(grep("dev", jags))>0, FALSE)
 
@@ -91,7 +100,7 @@ testthat::test_that("test.mb.write", {
 
 
   jags <- mb.write(fun=tfpoly(degree=2, pool.1="abs", method.1="common",
-                              pool.2="rel", method.2="random", method.power1="common", method.power2="random"),
+                              pool.2="rel", method.2="random", method.power1=1, method.power2=-1),
                    rho="dunif(-1,1)", covar="AR1")
   testthat::expect_equal(length(grep("beta\\.1", jags))>0, TRUE)
   testthat::expect_equal(length(grep("d\\.1", jags))>0, FALSE)
@@ -101,8 +110,8 @@ testthat::test_that("test.mb.write", {
   testthat::expect_equal(length(grep("sd\\.1", jags))>0, FALSE)
   testthat::expect_equal(length(grep("sd\\.beta\\.2", jags))>0, TRUE)
   testthat::expect_equal(length(grep("sd\\.power1", jags))>0, FALSE)
-  testthat::expect_equal(length(grep("sd\\.power2", jags))>0, TRUE)
-  testthat::expect_equal(length(grep("rho ~ dunif\\(-1,1", jags))>0, TRUE)
+  testthat::expect_equal(length(grep("sd\\.power2", jags))>0, FALSE)
+  testthat::expect_equal(length(grep("rho ~ dunif\\(-?[0-9],1", jags))>0, TRUE)
   testthat::expect_equal(length(grep("cor\\[i,r,c\\]", jags))>0, TRUE)
   testthat::expect_equal(length(grep("dev", jags))>0, FALSE)
 
