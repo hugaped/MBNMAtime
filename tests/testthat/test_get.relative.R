0aaf8b75c80c5af3061274b28b0406d451dd18ca
Hugo Pedder <hugo.pedder@bristol.ac.uk>
2022-08-27 14:28:40 +0100

    tests pass

--- tests/testthat/test_get.relative.R
+++ tests/testthat/test_get.relative.R
@@ -1,54 +1,120 @@
 testthat::context("Testing get.relative")
 
+datalist <- list(osteopain=osteopain, copd=copd, goutSUA_CFBcomb=goutSUA_CFBcomb,
+                 hyalarthritis=hyalarthritis, diabetes=diabetes, alog_pcfb=alog_pcfb)
 
-testthat::test_that("get.relative tests pass correctly", {
-  skip_on_ci()
-  skip_on_cran()
+n.iter <- 2000
 
-  testthat::test_that("get.relative passes", {
+for (i in 2:length(datalist)) {
 
-    alognet <- mb.network(alog_pcfb)
-    copdnet <- mb.network(copd)
+  print(names(datalist)[i])
 
-    itp <- mb.run(copdnet, titp(), corparam = FALSE, n.iter=2000)
-    loglin <- mb.run(alognet, tloglin(), n.iter=2000)
+  network <- mb.network(datalist[[i]])
+
+  testthat::test_that(paste0(names(datalist)[i], ": get.relative tests pass correctly"), {
+
+    skip_on_ci()
+    skip_on_cran()
+    skip_on_appveyor()
+
+    if (names(datalist)[i] %in% c("goutSUA_CFBcomb", "hyalarthritis", "alog_pcfb")) {
+      itp <- mb.run(network, tpoly(degree=2), corparam = FALSE, n.iter=n.iter)
+    } else {
+      itp <- mb.run(network, titp(), corparam = FALSE, n.iter=n.iter)
+    }
+
+    loglin <- mb.run(mb.network(datalist[[i-1]]), tloglin(), n.iter=n.iter)
 
     expect_error(get.relative(mbnma=loglin, mbnma.add=itp, time=20),
                  "mbnma and mbnma.add must have a single treatment")
 
 
-    copdnew <- copd
-    levels(copdnew$treatment)[1] <- "placebo"
-    copdnet <- mb.network(copdnew)
+    # Create new network with same treatment
+    netnew <- datalist[[i-1]]
+    if (class(datalist[[i-1]]$treatment) != class(datalist[[i]]$treatment)) {
+
+      netnew <- datalist[[i-2]]
+
+    }
+
+    if (is.factor(netnew$treatment)) {
+      levels(netnew$treatment)[1] <- itp$network$treatments[1]
+    } else if (is.character(netnew$treatment)) {
+      netnew$treatment[netnew$treatment==netnew$treatment[1]] <-
+        itp$network$treatments[1]
+    } else if (is.numeric(netnew$treatment)) {
+      netnew$treatment[netnew$treatment==netnew$treatment[1]] <-
+        as.numeric(itp$network$treatments[1])
+    }
 
 
-    itp <- mb.run(copdnet, titp(), corparam = FALSE)
-    loglin <- mb.run(alognet, tloglin(), n.iter=2000)
+    netnew <- mb.network(netnew)
+
+    loglin <- mb.run(netnew, tloglin(), n.iter=n.iter)
+
 
     expect_error(get.relative(mbnma=loglin, mbnma.add=itp), NA)
 
     expect_error(get.relative(mbnma=loglin, mbnma.add=itp, time=200), NA)
 
-    treats <- c("placebo", "alog_100", "Aclidinium")
+    treats <- c(network$treatments[1:2], netnew$treatments[3])
     rels <- get.relative(mbnma=loglin, mbnma.add=itp, treats=treats)
-    expect_equal(treats, rownames(rels$mean))
+    expect_equal(any(is.na(match(treats, rownames(rels$mean)))), FALSE)
 
-    treats <- c("alog_100", "placebo", "Aclidinium")
+    treats <- c(netnew$treatments[3], network$treatments[c(1,3)])
     rels <- get.relative(mbnma=loglin, mbnma.add=itp, treats=treats)
-    expect_equal(c("placebo", "alog_100", "Aclidinium"), rownames(rels$mean))
+    expect_equal(c(network$treatments[1],
+                   netnew$treatments[3],
+                   network$treatments[3]),
+                 rownames(rels$mean))
 
-    expect_error(get.relative(mbnma=loglin, mbnma.add=itp, treats=c("alog_100", "Aclidinium")),
+    expect_error(get.relative(mbnma=loglin, mbnma.add=itp,
+                              treats=c(network$treatments[2], netnew$treatments[3])),
                  "mbnma and mbnma.add must have a single treatment")
 
 
     # Test performing MBNMA with a different reference treatment for alog and check again
-    alognet2 <- mb.network(alog_pcfb, reference="alog_100")
-    loglin2 <- mb.run(alognet2, tloglin(), n.iter=2000)
+    ref <- ifelse(is.numeric(datalist[[i]]$treatment),
+                  as.numeric(network$treatments[3]),
+                  network$treatments[3])
 
-    treats <- c("alog_100", "placebo", "Aclidinium")
-    rels <- get.relative(mbnma=loglin2, mbnma.add=itp, treats=treats)
-    expect_equal(rownames(rels$mean), c("placebo", "alog_100", "Aclidinium"))
+    netref <- mb.network(datalist[[i]], reference=ref)
+    loglin2 <- mb.run(netref, tloglin(), n.iter=n.iter)
+
+
+    # Create new network with same treatment
+    netnew <- datalist[[i-1]]
+    if (class(datalist[[i-1]]$treatment) != class(datalist[[i]]$treatment)) {
+      netnew <- datalist[[i-2]]
+    }
+
+    if (is.factor(netnew$treatment)) {
+      levels(netnew$treatment)[1] <- loglin2$network$treatments[1]
+    } else if (is.character(netnew$treatment)) {
+      netnew$treatment[netnew$treatment==netnew$treatment[1]] <-
+        loglin2$network$treatments[1]
+    } else if (is.numeric(netnew$treatment)) {
+      netnew$treatment[netnew$treatment==netnew$treatment[1]] <-
+        as.numeric(loglin2$network$treatments[1])
+    }
+
+    netnew <- mb.network(netnew)
+
+    if (names(datalist)[i-1] %in% c("diabetes")) {
+
+      # WARNING CAN BE REMOVED AFTER v0.2.2
+      itp2 <- suppressWarnings(mb.run(netnew, temax(), corparam = TRUE, n.iter=n.iter))
+    } else {
+      itp2 <- mb.run(netnew, titp(), corparam = TRUE, n.iter=n.iter)
+    }
+
+
+    treats <- c(netnew$treatments[3], netref$treatments[c(1,3)])
+    rels <- get.relative(mbnma=loglin2, mbnma.add=itp2, treats=treats)
+    expect_equal(any(is.na(match(treats, rownames(rels$mean)))), FALSE)
 
   })
 
-})
+}
+
+
